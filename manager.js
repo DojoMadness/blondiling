9b78ed0d23461cb66fed77b1cb0cd1e93d7cce95
William <williamferk@gmail.com>
2019-08-30 13:47:07 +0200

    my last commit.

--- manager.js
+++ manager.js
@@ -1,36 +1,29 @@
 var Queue = require('bull');
 
 try {
-    console.log("blondling:manager@@")
-
     const channels = [
         "https://www.youtube.com/channel/UCBNcq7Kdw1F9LQ1zRpjeeWw",
-        "https://www.youtube.com/channel/UCERpgysn81hv0c0FXIjXsTg",
-        "https://www.youtube.com/channel/UCZprw7Bxzfh2IysXNnl1S3g",
-        "https://www.youtube.com/channel/UCvzoHz_9Vym9HUJ3lNRfCcw",
-        "https://www.youtube.com/channel/UCvSJMygwBMDEH6jxIKMdT1Q",
-        "https://www.youtube.com/channel/UCek8zsIojbAidIscsWK-vhQ",
-        "https://www.youtube.com/channel/UCgKshtw1k7w0Yw8lh41jMMQ",
-        "https://www.youtube.com/channel/UCD55o2LYTynwAp5JomjsEow"
     ]
 
     const youtubeChannelsProcess = new Queue('youtube-channels-process', 'redis://redis:6379')
     const youtubeChannelsResult = new Queue('youtube-channels-result', 'redis://redis:6379')
-    
 
     const results = []
 
-    youtubeChannelsResult.process(function(job, done) {
+    youtubeChannelsResult.process(async (job, done) => {
+        console.log(`manager:result`)
+        console.log(job.data)
         results.push(job.data)
-        done()
     })
 
+    var i = 0
     channels.forEach(function(channel) {
 
         console.log(`adding channel ${channel} to queue...`)
 
         youtubeChannelsProcess.add({
-            url: channel
+            url: channel,
+            seq: ++i
         })
     })
 
--- worker.js
+++ worker.js
@@ -1,19 +1,45 @@
-var Queue = require('bull');
+const Queue = require('bull');
+const puppeteer = require('puppeteer');
 
-try {
-    console.log("blondling:worker@@")
+(async () => {
+    try {
+        const browser = await puppeteer.launch({
+            headless: true,
+            args: [
+                '--lang=en',
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+            ]
+        })
 
-    const channelsProcess = new Queue('youtube-channels-process', 'redis://redis:6379')
-    const channelsResult = new Queue('youtube-channels-result', 'redis://redis:6379')
+        const channelsProcess = new Queue('youtube-channels-process', 'redis://redis:6379')
+        const channelsResult = new Queue('youtube-channels-result', 'redis://redis:6379')
 
-    channelsProcess.process(function(job, done) {
+/*
 
-        console.log(`processing channel...`)
-        const chuncks = job.data.url.split("/")
-        const name = chuncks[chuncks.length - 1]
-        channelsResult.add({ name: name })
-        done()
-    })
-} catch (ex) {
-    console.error(ex)
-}
+    The next step I was working on is to move the code from the t-blondi project to another file and call it like that:
+
+    queue.process(5, './YoutubeProcessor.js');
+*/
+
+        channelsProcess.process(async (job, done) => {
+    
+            const url = job.data.url
+
+            const channelsPage = await browser.newPage()
+            await channelsPage.goto(url)
+
+            console.log(`processing channel...${url}`)
+            const chuncks = url.split("/")
+
+            const name = chuncks[chuncks.length - 1]
+            console.log(`channel id: ${name}`)
+
+            channelsResult.add({ name: name, seq: job.data.seq })
+            await channelsPage.screenshot({path: `screenshots/${name}.png`})
+            console.log(`screenshots/${name}.png`)
+        })
+    } catch (ex) {
+        console.error(ex)
+    }
+})()
